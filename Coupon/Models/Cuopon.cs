f3b89c6d0418caed7da8ca534ad37b1ae030e567
Carlos Aucancela Maguana <carlos.aucancelam@perficient.com>
2026-02-13 15:37:31 -0500

    Created solution

---
+++ Coupon/Models/Cuopon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class Cuopon
+    {
+        [Key]
+        public int CuoponId { get; set; }
+        [Required]
+        public string CuoponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Coupon/Models/Dto/CuoponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class CuoponDto
+    {
+        public int CuoponId { get; set; }
+        public string CuoponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
