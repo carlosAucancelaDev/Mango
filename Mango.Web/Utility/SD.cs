46c3a011a5a3f55dae169470431701a23001560d
carlos.aucancelam <carlos.aucancelam@LATAM-1MK8573>
2026-02-14 21:17:21 -0500

    create a common - Base service to CRUD methods

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD //sharingDetails
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
