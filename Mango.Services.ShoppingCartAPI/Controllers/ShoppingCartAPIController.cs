0b31ad73eddfac8f4980e522ff07e46fa1931812
carlos.aucancelam <carlos.aucancelam@LATAM-1MK8573>
2026-02-19 19:29:36 -0500

    Added add and remove Coupon endpoints in cart shopping

--- Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
@@ -23,43 +23,81 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             this._response = new ResponseDto();
         }
 
-        [HttpGet("GetCart/{userId}")]
-        public async Task<ResponseDto> GetCart(string userId)
+        //[HttpGet("GetCart/{userId}")]
+        //public async Task<ResponseDto> GetCart(string userId)
+        //{
+        //    try
+        //    {
+        //        CartDto cart = new()
+        //        {
+        //            CartHeader = _mapper.Map<CartHeaderDto>(_db.CartHeaders.First(u => u.UserId == userId))
+        //        };
+        //        cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails
+        //            .Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId));
+
+        //        IEnumerable<ProductDto> productDtos = await _productService.GetProducts();
+
+        //        foreach (var item in cart.CartDetails)
+        //        {
+        //            item.Product = productDtos.FirstOrDefault(u => u.ProductId == item.ProductId);
+        //            cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+        //        }
+
+        //        //apply coupon if any
+        //        if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
+        //        {
+        //            CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
+        //            if (coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
+        //            {
+        //                cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+        //                cart.CartHeader.Discount = coupon.DiscountAmount;
+        //            }
+        //        }
+
+        //        _response.Result = cart;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _response.IsSuccess = false;
+        //        _response.Message = ex.Message;
+        //    }
+        //    return _response;
+        //}
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
         {
             try
             {
-                CartDto cart = new()
-                {
-                    CartHeader = _mapper.Map<CartHeaderDto>(_db.CartHeaders.First(u => u.UserId == userId))
-                };
-                cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails
-                    .Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId));
-
-                IEnumerable<ProductDto> productDtos = await _productService.GetProducts();
-
-                foreach (var item in cart.CartDetails)
-                {
-                    item.Product = productDtos.FirstOrDefault(u => u.ProductId == item.ProductId);
-                    cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
-                }
-
-                //apply coupon if any
-                if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
-                {
-                    CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
-                    if (coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
-                    {
-                        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
-                        cart.CartHeader.Discount = coupon.DiscountAmount;
-                    }
-                }
-
-                _response.Result = cart;
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
             }
             catch (Exception ex)
             {
                 _response.IsSuccess = false;
-                _response.Message = ex.Message;
+                _response.Message = ex.ToString();
+            }
+            return _response;
+        }
+
+        [HttpPost("RemoveCoupon")]
+        public async Task<object> RemoveCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();
             }
             return _response;
         }
--- Mango.Services.ShoppingCartAPI/Service/ProductService.cs
+++ Mango.Services.ShoppingCartAPI/Service/ProductService.cs
@@ -4,25 +4,25 @@ using System.Text.Json.Serialization;
 
 namespace Mango.Services.ShoppingCartAPI.Service
 {
-    public class ProductService : IProductService
-    {
-        private readonly IHttpClientFactory _httpClientFactory;
+    //public class ProductService : IProductService
+    //{
+    //    private readonly IHttpClientFactory _httpClientFactory;
 
-        public ProductService(IHttpClientFactory clientFactory)
-        {
-            _httpClientFactory = clientFactory;
-        }
-        public async Task<IEnumerable<ProductDto>> GetProducts()
-        {
-            var client = _httpClientFactory.CreateClient("Product");
-            var response = await client.GetAsync($"/api/product");
-            var apiContet = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContet);
-            if (resp.IsSuccess)
-            {
-                return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(Convert.ToString(resp.Result));
-            }
-            return new List<ProductDto>();
-        }
-    }
+    //    public ProductService(IHttpClientFactory clientFactory)
+    //    {
+    //        _httpClientFactory = clientFactory;
+    //    }
+    //    //public async Task<IEnumerable<ProductDto>> GetProducts()
+    //    //{
+    //    //    var client = _httpClientFactory.CreateClient("Product");
+    //    //    var response = await client.GetAsync($"/api/product");
+    //    //    var apiContet = await response.Content.ReadAsStringAsync();
+    //    //    var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContet);
+    //    //    if (resp.IsSuccess)
+    //    //    {
+    //    //        return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(Convert.ToString(resp.Result));
+    //    //    }
+    //    //    return new List<ProductDto>();
+    //    //}
+    //}
 }
